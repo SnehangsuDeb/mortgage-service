c867c706e91427ea44df328a5d36893e40567858
sneha <snehangsu.deb@gmail.com>
2025-10-13 16:39:15 +0530

    Initial commit

---
+++ src/test/java/com/example/mortgageservice/MortgageServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.mortgageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
